d8d125cac0a77526db5f8f9063db323ccb609375
Preecha-M <preecha.sr@kkumail.com>
2026-02-23 23:04:53 +0700

    fix crud

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: true,
+    origin: ['http://localhost:5173', 'https://dbms-pro-j0lm.onrender.com'],
     credentials: true,
   });
 
